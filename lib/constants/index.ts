676e9ebbe8b3da7989ccc8a13b01f833de389832
John Fuller <fuller.john84@gmail.com>
2025-03-16 20:00:20 -0400

    First commit

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "ProStore";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "A modern Ecommerce platform built using Next.js";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
