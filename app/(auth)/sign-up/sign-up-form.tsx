02b84c3a45d922745ecdfee171f7df62b5645724
John Fuller <fuller.john84@gmail.com>
2025-03-20 14:17:42 -0400

    Added SignUp error handling.

--- app/(auth)/sign-up/sign-up-form.tsx
+++ app/(auth)/sign-up/sign-up-form.tsx
@@ -39,7 +39,7 @@ const SignUpForm = () => {
             id="name"
             name="name"
             type="text"
-            required
+            // required
             autoComplete="name"
             defaultValue={signUpDefaultValues.name}
           />
@@ -50,7 +50,7 @@ const SignUpForm = () => {
             id="email"
             name="email"
             type="email"
-            required
+            // required
             autoComplete="email"
             defaultValue={signUpDefaultValues.email}
           />
